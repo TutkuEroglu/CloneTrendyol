8257a46b254e63967002dcbd33d3fa5e9461c5ca
TutkuEroglu <rrandqua@gmail.com>
2022-12-05 04:30:18 +0300

    added lists

---
+++ src/ArrayList/List.js
@@ -0,0 +1,254 @@
+export const BrandList = [
+  {
+    id:1,
+    text:"Nike",
+  },
+  {
+    id: 2,
+    text: "Defacto",
+  },
+  {
+    id:3,
+    text:"TRENDYOLMILLA",
+  }
+  ,
+  {
+    id:4,
+    text:"adidas",
+  }
+  ,
+  {
+    id:5,
+    text:"Koton",
+  }
+  ,
+  {
+    id:6,
+    text:"Pull & Bear",
+  }
+  ,
+  {
+    id:7,
+    text:"Addax",
+  }
+  ,
+  {
+    id:8,
+    text:"Mavi",
+  }
+  ,
+  {
+    id:9,
+    text:"Puma",
+  },
+  {
+    id:10,
+    text:"HUMMEL",
+  }
+  ,
+  {
+    id:11,
+    text:"Stradivarius",
+  },
+  {
+    id:12,
+    text:"LC Waikiki",
+  }
+  ,
+  {
+    id:13,
+    text:"TRENDYOL MAN",
+  }
+]
+
+export const BodySize = [
+  {
+    id:1,
+    text:"XXS"
+  },
+  {
+    id:2,
+    text:"XS"
+  },
+  {
+    id:3,
+    text:"S"
+  },
+  {
+    id:4,
+    text:"M"
+  },
+  {
+    id:5,
+    text:"L"
+  },
+  {
+    id:6,
+    text:"XL"
+  },
+  {
+    id:7,
+    text:"XXL"
+  },
+  {
+    id:8,
+    text:"2XL"
+  },
+  {
+    id:9,
+    text:"3XL"
+  },
+  {
+    id:10,
+    text:"4XL"
+  },
+]
+
+export const Colours = [
+  {
+    id:1,
+    colorText:"#F5E0CC"
+  },
+  {
+    id:2,
+    colorText:"#FFFFFF"
+  },
+  {
+    id:3,
+    colorText:"#A52C36"
+  },
+  {
+    id:4,
+    colorText:"#EDE9DE"
+  },
+  {
+    id:5,
+    colorText:"#DDDDDD"
+  },
+  {
+    id:6,
+    colorText:"#8B9955"
+  },
+  {
+    id:7,
+    colorText:"#8B572A"
+  },
+  {
+    id:8,
+    colorText:"#DE001B"
+  },
+  {
+    id:9,
+    colorText:"#1F3897"
+  },
+  {
+    id:10,
+    colorText:"#71B2FF"
+  },
+  {
+    id:11,
+    colorText:"#8331CC"
+  },
+  {
+    id:12,
+    colorText:"#FFA8E7"
+  },
+  {
+    id:13,
+    colorText:"#FFE500"
+  },
+  {
+    id:14,
+    colorText:"#565656"
+  },
+  {
+    id:15,
+    colorText:"#46C49E"
+  },
+  {
+    id:16,
+    colorText:"#F5A623"
+  },
+  {
+    id:17,
+    colorText:"#7ED321"
+  },
+  {
+    id:18,
+    colorText:"linear-gradient(90deg, #fa4750 0%, #e24597 13%, #a550ff 25%, #6c7ff8 38%, #70bbfd 50%, #2effd4 61%, #57e4a0 73%, #d2ab48 86%, #fdde0a 100%)"
+  },
+]
+
+export const PriceList = [
+  {
+    id:1,
+    text:"0 TL - 125 TL"
+  },
+  {
+    id:2,
+    text:"125 TL - 200 TL"
+  },
+  {
+    id:3,
+    text:"200 TL - 350 TL"
+  },
+  {
+    id:4,
+    text:"350 TL - 500 TL"
+  },
+  {
+    id:5,
+    text:"500 TL - 1250 TL"
+  },
+  {
+    id:6,
+    text:"1250 TL - 15000 TL"
+  }
+]
+
+export const StarProductList = [
+  {
+    id:1,
+    text:"Üç Yıldızlı Ürün"
+  },
+  {
+    id:2,
+    text:"İki Yıldızlı Ürün"
+  },{
+    id:3,
+    text:"Bir Yıldızlı Ürün"
+  },
+]
+
+export const TrotterType = [
+  {
+    id:1,
+    text:"Dar Paça"
+  },
+  {
+    id:2,
+    text:"Fermuarlı Paça"
+  },{
+    id:3,
+    text:"Lastikli Paça"
+  },
+  {
+    id:4,
+    text:"Normal Paça"
+  },
+  {
+    id:5,
+    text:"Jogger"
+  },{
+    id:6,
+    text:"Boru Paça"
+  },
+  {
+    id:7,
+    text:"Lastiksiz Paça"
+  },
+  {
+    id:8,
+    text:"Geniş Paça"
+  }
+]
